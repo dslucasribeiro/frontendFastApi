3c0547c5358eddf12b681e248861f20a9dacb51c
dslucasribeiro <dslucasribeiro@gmail.com>
2024-03-05 22:00:42 -0300

    Styles: Template modal alterado.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { ListaProdutosComponent } from './paginas/lista-produtos/lista-produtos.component';
 import { FormularioProdutoComponent } from './paginas/formulario-produto/formulario-produto.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'produtos', pathMatch: 'full'},
-    { path: 'home', component: HomeComponent },
     { path: 'produtos', component: ListaProdutosComponent },
     { path: 'formulario', component: FormularioProdutoComponent},
 ];
